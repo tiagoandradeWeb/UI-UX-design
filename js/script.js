ee9e27d84d00589b600b642f021b75cc9979d923
Tiago Andrade <andrade.tiago166@gmail.com>
2020-11-02 19:57:38 -0300

    Seção portifolio adicionada

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-let show = true;
+function menuMobile(){
+   let show = true;
 
 const nav = document.querySelector('.nav');
 const menuToggle = document.querySelector('.menu-toggle');
@@ -6,4 +7,29 @@ const menuToggle = document.querySelector('.menu-toggle');
 menuToggle.addEventListener('click', () =>{
 	nav.classList.toggle("on", show);
 	show = !show;
-})
+}) 
+}
+
+function modalImage(){
+    const imgs = document.querySelectorAll(".box-img img");
+    const view = document.querySelectorAll(".view")
+    const modalBox = document.querySelector(".img-modal")
+    const modalImg = document.querySelector(".img-modal img");
+    const btnModalClose = document.querySelector(".btn-modal-close");
+    
+    imgs.forEach(function(item){
+        item.addEventListener("click", function(){
+            const srcImg= item.getAttribute("src");
+            modalImg.setAttribute("src", srcImg);
+            modalBox.style.display="flex";
+            btnModalClose.addEventListener("click", function(){
+               modalBox.style.display="none"; 
+            });
+            
+    
+    })
+                 });
+}
+
+menuMobile();
+modalImage();
